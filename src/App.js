e4efc42bdd08667483e979640ed448c36b1ec259
Your Name <mareliza16@hotmail.com>
2022-08-04 06:07:27 -0500

    se crea el componente funcional Navbar y se hace el menu de navegación

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import "./App.css";
 import Home from "./components/Home.js";
 import NewMovies from "./components/NewMovies";
 import DescriptionMovie from "./components/DescriptionMovie";
+import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
       <Router>
         <Routes>
+          <Route path="/" element={<Navbar />} />
+          <Route path="/inicio" element={<Home />} />
           <Route path="/newMovies" element={<NewMovies />} />
           <Route path="/descriptionMovie" element={<DescriptionMovie />} />
-          <Route path="/" element={<Home />} />
         </Routes>
       </Router>
     </>
